dcc3846d28574d7b38227774680bc2015edbf001
Łukasz <riraseth@gmail.com>
2020-02-25 22:10:01 +0100

    initial setup

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+const ExpenseList = () => {
+  return (
+    <div className="">
+      hello from expense item
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+};
+
+export default ExpenseList;
